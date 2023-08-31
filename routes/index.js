869dc6ca263ce75bae87e6434a289a6057d0e862
Baran Dogan <phscloq@gmail.com>
2023-08-31 13:09:13 +0300

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  
+  res.render('index', { title: 'Mini Messageboard', messages:messages });
+});
+
+router.get('/new', function(req, res){
+
+res.render('form', {title: 'Post'});
+});
+
+router.post('/new', function(req, res){
+  messages.push({text: req.body.messageText, user: req.body.messageUser, added: new Date()});
+  res.redirect('/');
+
+})
+
+module.exports = router;
